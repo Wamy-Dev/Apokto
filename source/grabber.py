d466cdba041349bccec856cf3cffb094179cabb9
David <25761442+Wamy-Dev@users.noreply.github.com>
2022-04-16 02:49:13 -0500

    added random ids for searching in the future

--- source/grabber.py
+++ source/grabber.py
@@ -5,10 +5,12 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import decouple
 from decouple import config
+import random
+import string
 
 #env variables
-CERTLOCATION = config('CERTLOCATION', default="./firebasecert.json")
-DBLOCATION = config('DBLOCATION', default="repos")
+CERTLOCATION = config('CERTLOCATION')
+DBLOCATION = config('DBLOCATION')
 #json
 json_data = {}
 #firebase
@@ -16,12 +18,13 @@ cred = credentials.Certificate(CERTLOCATION)
 response = requests.get("https://api.parcility.co/db/repos")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-
 #
 if response.status_code == 200:
 	print(f'Status code: {response.status_code}. Getting all repos available.')
 	data = response.json()["data"]
 	for each in data:
+		N=10
+		ID= ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
 		try:
 			name = each["Label"]
 			name = name.encode("utf-8").decode("utf-8")
@@ -48,6 +51,7 @@ if response.status_code == 200:
 		try:
 			upload = db.collection(DBLOCATION).document(name)
 			upload.set({
+				'id': ID,
 				'repo': repo,
 				'icon': icon,
 				'desc': description
@@ -56,17 +60,18 @@ if response.status_code == 200:
 			#only if the original name is invalid for whatever reason
 			upload = db.collection(DBLOCATION).document(altname)
 			upload.set({
+				'id': ID,
 				'repo': repo,
 				'icon': icon,
 				'desc': description
 				}, merge=True)
 		#to json file
 		json_data[name] = {}
-		json_data[name] = {"repo": repo, "icon": icon, "desc": description}
+		json_data[name] = {"id": ID, "repo": repo, "icon": icon, "desc": description}
 	json_data = json.dumps(json_data)
 	json_file = open("repos.json", "w")
 	json_file.write(json_data)
 	json_file.close()
 	print('Finished process.')
 else:
-	print("API is down.")
+	print("API is down. Process cannot continue.")
