043a1ea93a41e4de9691cfc40267ae83942221b2
David <25761442+Wamy-Dev@users.noreply.github.com>
2022-06-13 13:22:47 -0500

    fixed backend

--- source/grabber.py
+++ source/grabber.py
@@ -3,9 +3,10 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import decouple
+import random
 from decouple import config
 from datetime import datetime
+import string
 #env variables
 CERTLOCATION = config('CERTLOCATION')
 DBLOCATION = config('DBLOCATION')
@@ -23,22 +24,24 @@ if response.status_code == 200:
 		docs = coll_ref.limit(batch_size).stream()
 		deleted = 0
 		for doc in docs:
-			print(f'Deleting doc {doc.id} => {doc.to_dict()}')
+			print('Deleting previous docs... please wait.')
 			doc.reference.delete()
 			deleted = deleted + 1
 		if deleted >= batch_size:
 			return delete_collection(coll_ref, batch_size)
 	delete_collection(db.collection(DBLOCATION), 1000)
-	print(f'Status code: {response.status_code}. Getting all repos available.')
+	print(f'Parcility status: {response.status_code}. Getting all repos available.')
 	data = response.json()["data"]
 	for each in data:
+		N = 10#sets to a 10 digit random string
+		ID = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
 		#testing
 		try:
 			testrepo = each["repo"]
 			reporesponse = requests.get(testrepo, timeout=5)
-			print(f'reporesponse={reporesponse.status_code}')
+			print(f'Repo Status={reporesponse.status_code}')
 		except:
-			print('Failed repo. Skipping\n##########')
+			print(f'Repo Status={reporesponse.status_code}. Skipping\n##########')
 		#if valid
 		if reporesponse.status_code == 200:
 			#getting repo names
@@ -48,17 +51,14 @@ if response.status_code == 200:
 				name = name.replace("\r", "")
 				altname = each["id"]
 				print(name)
-			except: 
-				name = "Name unknown"
-				print(name)
+			except:
+				name = "Name Unknown"
 			finally:
 				#getting repo links
 				try:
 					repo = each["repo"]
-					print(repo)
 				except:
-					repo = "Repo unknown"
-					print(repo)
+					repo = "Repo Unknown"
 				finally:
 					#getting repo icons
 					try:
@@ -66,47 +66,49 @@ if response.status_code == 200:
 						iconresponse = requests.get(testicon, timeout=5)
 						if iconresponse.status_code == 200:
 							icon = testicon
-							print(f'iconresponse={iconresponse.status_code}')
+							print(f'Icon Status={iconresponse.status_code}. Using provided icon.')
 						else:
 							icon = "https://github.com/Wamy-Dev/Apokto/blob/main/assets/error.png?raw=true"
-							print(icon)
+							print(f'Icon Status={iconresponse.status_code}. Failed, using default icon.')
 					except:
 						icon = "https://github.com/Wamy-Dev/Apokto/blob/main/assets/error.png?raw=true"
-						print(f'iconresponse={iconresponse.status_code}. getting icon failed, using fallback')
+						print(f'Icon Status={iconresponse.status_code}. Failed, using default icon.')
 					finally:
 						#getting repo descriptions
 						try: 
 							description = each["Description"]
 							description = description.encode("utf-8").decode("utf-8", "ignore")
 							description = description.replace("\r", "").replace(u"\uFFFD", "").replace('"', "")
-							print(description)
 						except:
-							description = "Description unknown"
-							print(description)
+							description = "Description Unknown"
 						finally:
-							print("done with this valid repo")
+							print("Finished valid repo.")
 			try:
 				upload = db.collection(DBLOCATION).document(name)
 				upload.set({
+					'id': ID,
+					'name': name,
 					'repo': repo,
 					'icon': icon,
 					'desc': description
 					}, merge=True)
-				print("uploaded using original name.\n##########")
+				print("Added repo to db using default name.\n##########")
 			except:
 				#only if the original name is invalid for whatever reason
 				upload = db.collection(DBLOCATION).document(altname)
 				upload.set({
+					"name": altname,
+					'id': ID,
 					'repo': repo,
 					'icon': icon,
 					'desc': description
 					}, merge=True)
-				print("uploaded using alt name.\n##########")
+				print("Added repo to db using alternate name.\n##########")
 			#to json file
-			json_data[name] = {}
-			json_data[name] = {"repo": repo, "icon": icon, "desc": description}
+			json_data[name or altname] = {}
+			json_data[name or altname] = {"repo": repo, "icon": icon, "desc": description, "id": ID}
 		else:
-			print("Failed repo, skipping.\n##########")	
+			print(f'Repo Status={reporesponse.status_code}. Skipping\n##########')	
 	json_data = json.dumps(json_data)
 	json_file = open("repos.json", "w")
 	json_file.write(json_data)
@@ -119,4 +121,4 @@ if response.status_code == 200:
 	except requests.RequestException as e:
 		print("Ping failed: %s" % e)
 else:
-	print("API is down.")
+	print("api.parcility.co is down.")
